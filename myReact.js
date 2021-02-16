b553a22bebaa5aea6288cbf8d9c6b7a1b85c56a0
Martin Smitnya <smitnyamartin@gmail.com>
2021-02-16 13:44:44 +0100

    basic react setup NO  create-react-app

---
+++ myReact.js
@@ -0,0 +1,16 @@
+// npm init
+// npm install -g create-react-app
+//...
+/*IN HTML:
+<script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
+*/
+
+const reactContentRoot = document.getElementById('root');
+
+ReactDOM.render('Hello', reactContentRoot);
+
+
+//...OR
+// create-react-app myAppName
+console.log(ReactDOM)
