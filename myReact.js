a51e821f1809be701c7b64b9377f4154eff18966
Martin Smitnya <smitnyamartin@gmail.com>
2021-02-16 17:33:38 +0100

    Babel also configured but no imedaite update yet

--- myReact.js
+++ myReact.js
@@ -1,13 +1,36 @@
 // npm init
 // npm install -g create-react-app
-//...
+// create-react-app myPATH
+
+
+//...OR...
 /*IN HTML:
 <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
 <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
+  //Babel <script src="https://unpkg.com/@babel/standalone/babel.min.js" crossorigin></script>
 */
 
-const reactContentRoot = document.getElementById('root');
-ReactDOM.render('Hello there', reactContentRoot);
 
-//...OR
-// create-react-app myAppName
+const reactContentRoot = document.getElementById('root');
+ReactDOM.render(<App />, reactContentRoot);
+
+function App() {
+  let cookies = 0;
+  function handleBuy() {
+    cookies++;
+    console.log(cookies)
+  }
+
+  function handleEat() {
+    if (cookies > 0) {
+      cookies--
+    }
+  }
+  return (
+    <div>
+      <button onClick={handleBuy} >  Buy one </button>
+      <p>{cookies}</p>
+      <button onClick={handleEat} >  Eat one </button>
+    </div>
+  );
+}
