a6f7b1598fe04c54f44cc0513d4d0ea21a804604
Martin Smitnya <smitnyamartin@gmail.com>
2021-02-18 17:38:46 +0100

    buttons functional

--- beershop/src/App.js
+++ beershop/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
 
 function App() {
   return (
@@ -9,62 +8,60 @@ function App() {
     </div>
   );
 }
-
 export default App;
 
 
 
-
+//Components        Components        Components        Components        Components
 function Header() {
   return (<div>
     <h1>{'Welcome to our website!'}</h1>
   </div>)
 }
 
-
-
 function Beertitle() {
-  const [counter, setCounter] = useState(1);
+  const [page, setPage] = useState(1);
   const [beers, setBeers] = useState('No beers displayed')
-  const [describtion, setDescription] = useState(false);
+  const [currentElement, setCurrentElement] = useState(null)
 
   useEffect(() => {
-    fetch(`https://api.punkapi.com/v2/beers?page=${counter}`)
+    fetch(`https://api.punkapi.com/v2/beers?page=1`)
       .then(response => response.json()).then(data => setBeers(data))
       .catch(e => console.log(e))
 
   }, [])
+  
+  const totalBeers = [];
+  for (let i = 0; i < beers.length/5; i++) {
+    totalBeers.push(i+1)
+  }
+
   const beersArr = [];
-  for (let i = 0; i < beers.length; i++) {
+  for (let i = (page*5)-5; i < (page*5); i++) {
     beersArr.push(beers[i])
   }
-  //Handle click to setDescription
-  
-  //Handle click to setDescription
-
-
-
-
-
-  console.log(beersArr[0])
 
   return (<div>
     
     <div>
       {
         beersArr.map((element) => {
-          //Description or Image Logic
-          if(!describtion) {
+          //Description logic
+          if(currentElement === element.id) {
             return (
-              <div>
-                <img src={element.image_url} height='100px'  />
+              <div onClick={ ()=> {
+                setCurrentElement(null)
+              }}>
+                <p style={{"height" : "155px"}}>{element.description}</p>
                 <p>{element.name}</p>
               </div>
             )
           } else {
             return (
-              <div>
-                <p>{element.description}</p>
+              <div onClick={ ()=> {
+                setCurrentElement(element.id)
+              }}>
+                <img src={element.image_url} height='150px' />
                 <p>{element.name}</p>
               </div>
             )
@@ -72,6 +69,22 @@ function Beertitle() {
         })
       }
     </div>
-    <p>{counter}</p>
+
+    <div>
+      {//If we are NOT on first page -- else stay here
+      }
+      <button onClick={()=> {page>1? setPage(page-1) : setPage(page)} }>{'<<'}</button>
+      {
+        totalBeers.map( (element)=> {return(
+          //Page selector button
+          <button onClick={
+            ()=> {setPage(element)} 
+          }>{element}</button>
+        )})
+      }
+      <button onClick={()=> {page<totalBeers.length ? setPage(page+1) : setPage(page)} }>{'>>'}</button>
+      {//If we are NOT on last page ++ else stay here
+      }
+    </div>
   </div>)
 }
